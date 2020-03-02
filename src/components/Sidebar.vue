ae4bf7a80f45fe561c0dea11d3b58f071de42fec
Anton <270025112@acgedu.com>
2020-03-02 15:58:13 +1300

    Setup

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sidebar">
+    <h1>Group Portfolio</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.sidebar{
+    background-color: #1D2951;
+    max-width: 400px;
+    height: 100vh;
+    h1{
+        color: white;
+    }
+}
+</style>
