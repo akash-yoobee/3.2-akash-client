ae4aca2c483b9f7f11ce824ab90081389448114b
akash-yoobee <54612932+akash-yoobee@users.noreply.github.com>
2020-03-03 19:01:30 +1300

    Tue from school

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-      <h2>This is the home page</h2>
+  <div class="student-cards">
+    <button type="button">Anton</button>
+    <button type="button">Chris</button>
+    <button type="button">Akash</button>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.student-cards {
+  background-color: linear-gradient(
+    90deg,
+    rgba(238, 238, 238, 1) 0%,
+    rgba(221, 221, 221, 1) 35%,
+    rgba(198, 199, 200, 1) 100%
+  );
+  width: 100%;
+  height: 100vh;
+  button {
+    background-image: url("../assets/images/akash-background.jpg");
+    color: white;
+  }
+}
 </style>
