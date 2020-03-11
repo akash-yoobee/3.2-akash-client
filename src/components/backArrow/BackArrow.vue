d73977841c1d086473d1ead08026d51adfbea3fa
Anton <270025112@acgedu.com>
2020-03-11 18:49:58 +1300

    added more routing

---
+++ src/components/backArrow/BackArrow.vue
@@ -0,0 +1,20 @@
+<template>
+    <router-link :to="{name: link}">
+      <font-awesome-icon class="icon" icon="angle-left" />
+    </router-link>
+</template>
+
+<script>
+
+export default {
+  props: ["link"]
+};
+</script>
+
+<style lang="scss">
+.icon{
+      margin-right: 1rem;
+    font-size: 35px;
+    color: white;
+}
+</style>
