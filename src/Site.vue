a65877a2048b4bfc9c3daf503694a0c44f41eb41
Anton <270025112@acgedu.com>
2020-03-12 18:46:26 +1300

    added admin

---
+++ src/Site.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="site">
+    <Sidebar />
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "Site",
+  components: {
+    Sidebar
+  }
+};
+</script>
+
+<style lang="scss">
+@import "assets/scss/_variables";
+
+#site {
+  display: flex;
+  height: 100vh;
+}
+.content {
+  overflow-y: scroll;
+  background: $global-background;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import ProjectDetails from './components/ProjectDetails'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
@@ -22,30 +21,51 @@ Vue.config.productionTip = false
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
+import Site from './Site';
+import Admin from './Admin';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Pagespeed from './components/Pagespeed';
+import ProjectDetails from './components/ProjectDetails'
+import AdminLogin from './components/admin/AdminLogin'
+
 
 const routes = [{
-  name: 'home',
   path: '/',
-  component: Home
-},
-{
-  name: 'projects',
-  path: '/projects',
-  component: Projects
-},
-{
-  name: "pagespeed",
-  path: "/pagespeed",
-  component: Pagespeed
-},
-{
-  name: 'projectdetails',
-  path: '/projectdetails',
-  component: ProjectDetails
-}]
+  component: Site,
+  children:[{
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'projects',
+    path: '/projects',
+    component: Projects
+  },
+  {
+    name: "pagespeed",
+    path: "/pagespeed",
+    component: Pagespeed
+  },
+  {
+    name: 'projectdetails',
+    path: '/projectdetails',
+    component: ProjectDetails
+  }]
+},{
+    path: '/admin',
+  component: Admin,
+  children: [
+    {
+      name: 'adminLogin',
+      path: '/',
+      component: AdminLogin
+    }
+  ]
+}
+]
+
 
 const router = new VueRouter({
   routes
